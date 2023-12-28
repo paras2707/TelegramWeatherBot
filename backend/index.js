4f51b3be1e05409d3adbf1d645a9978b2405cc6e
Paras Thapar <parasthapar@192.168.1.6>
2023-12-28 17:24:16 +0530

    adding changes in backend for deployement

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,13 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://telegram-weather-bot-frontend.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // ------------------- API Variables ------------------- //
 let TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
