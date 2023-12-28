07bda6873aba6ea9cb7ad98c5b6978f37f58e4b4
Paras Thapar <parasthapar@192.168.1.6>
2023-12-28 18:08:35 +0530

    adding fixes

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "https://telegram-weather-bot-frontend.vercel.app",
-      "http://localhost:5173",
-    ],
+    origin: ["https://telegram-weather-bot-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
