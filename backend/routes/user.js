c2c1b558a0dfe637f20a327b5cb74984e001bc67
Paras Thapar <parasthapar@192.168.1.6>
2023-12-28 16:39:06 +0530

    adding initial files

---
+++ backend/routes/user.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const users = await User.find();
+  res.send(users);
+});
+
+router.delete("/:id", async (req, res) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+  res.send(user);
+});
+
+router.put("/:id", async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.params.id, {
+    isBlocked: req.body.isBlocked,
+  });
+  res.send(user);
+});
+
+router.post("/settings", async (req, res) => {
+  const keys = req.body;
+  console.log(keys);
+  res.send(keys);
+});
+
+module.exports = router;
